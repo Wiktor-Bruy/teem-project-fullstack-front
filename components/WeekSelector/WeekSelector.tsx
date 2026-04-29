56889e0626dae92476033a80f981763f47b7c6ca
Daria Kazarina <kdasa033@gmail.com>
2026-04-29 15:19:47 +0200

    commit for pull

--- components/WeekSelector/WeekSelector.tsx
+++ components/WeekSelector/WeekSelector.tsx
@@ -1,3 +1,15 @@
+import css from './WeekSelector.module.css';
+
 export default function WeekSelector() {
-  return <p>WeekSelector</p>;
+  
+  return (
+    <section className="section-weeks">
+      <div className="weeks-scroll-container">
+        <button className="button-week">
+          <p className="week-number">6</p>
+          <p className="week-name">Тиждень</p>
+        </button>
+      </div>
+    </section>
+  );
 }
--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -1,6 +1,29 @@
 import { cookies } from 'next/headers';
 import { nextServer } from './api';
 
+export interface BabyState {
+  _id: string;
+  analogy: boolean;
+  weekNumber: number;
+  babySize: number;
+  babyWeight: number;
+  image: string;
+  babyActivity: string;
+  babyDevelopment: string;
+  interestingFact: string;
+  momDailyTips: string[];
+}
+
+export interface Note {
+  _id: string;
+  title: string;
+  description: string;
+  emotions: string[];
+  userId: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
 export async function checkSession() {
   const cookieStore = await cookies();
 
@@ -19,8 +42,15 @@ export async function getMe() {}
 export async function getNotes() {}
 
 //------------------------------------------------Повертає-один-запис-щоденника-по-id
-export async function getNote() {}
-
+export async function getNote(noteId: string): Promise<Note> {
+  try {
+    const res = await nextServer.get<Note>(`/note/${noteId}`);
+    return res.data;
+  } catch (error) {
+    console.error('Error fetching note:', error);
+    throw error;
+  }
+}
 //------------------------------------------------Повертає-всі-таски
 export async function getTasks() {}
 
@@ -28,7 +58,15 @@ export async function getTasks() {}
 export async function getTerm() {}
 
 //------------------------------------------------Повертає-стан-дитини
-export async function getBabyState() {}
+export async function getBabyState(): Promise<BabyState> {
+  try {
+    const res = await nextServer.get<BabyState>('/home/baby');
+    return res.data;
+  } catch (error) {
+    console.error('Error fetching baby state:', error);
+    throw error;
+  }
+}
 
 //------------------------------------------------Повертає-стан-мами
 export async function getMomState() {}
