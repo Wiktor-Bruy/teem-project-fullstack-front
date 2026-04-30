cc2bf5216c82b4ada98c71434ab5a88d130b0984
Alina <alinazolotopup99@gmail.com>
2026-04-30 18:32:33 +0200

    review lib/clientApi

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,6 +1,32 @@
 import axios from 'axios';
+import { refreshSession } from './clientApi';
+
 
 export const nextServer = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
   withCredentials: true,
 });
+
+nextServer.interceptors.response.use(
+  res => res,
+  async (error) => {
+    const originalRequest = error.config;
+    
+    if (originalRequest.url === '/auth/refresh') {
+      return Promise.reject(error);
+    }
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        await refreshSession();
+        return nextServer(originalRequest);
+      } catch (e) {
+        window.location.href = '/auth/login';
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
