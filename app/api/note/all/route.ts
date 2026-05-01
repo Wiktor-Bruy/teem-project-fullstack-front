0153e4b0a8ed78e614b7a81170bf1ad493f91122
ankurzhos <kurzhos96@gmail.com>
2026-05-01 22:19:53 +0300

    add route getAllNotes

---
+++ app/api/note/all/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import { api } from '../../api';
+import { cookies } from 'next/headers';
+import { isAxiosError } from 'axios';
+
+export async function GET() {
+  try {
+    const cookieStore = await cookies();
+
+    const res = await api.get('/note', {
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+    });
+
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      return NextResponse.json(
+        { error: error.message, data: error.response?.data },
+        { status: error.status || 500 }
+      );
+    }
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    );
+  }
+}
