6d5ecb3a89b3fe8492b62f1fcf2f4c1f83587546
Crazy_Fox <holmyliszag@ukr.net>
2026-05-01 10:47:54 +0300

    add route emotions

---
+++ app/api/emotions/route.ts
@@ -0,0 +1,28 @@
+import { api } from '../api';
+import { isAxiosError } from 'axios';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    const cookieStore = await cookies();
+
+    const res = await api('/emotions', {
+      headers: {
+        Cookie: cookieStore.toString(),
+      },
+    });
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error) {
+    if (isAxiosError(error)) {
+      return NextResponse.json(
+        { error: error.message, data: error.response?.data },
+        { status: error.status }
+      );
+    }
+    return NextResponse.json(
+      { error: 'Some server error...' },
+      { status: 500 }
+    );
+  }
+}
