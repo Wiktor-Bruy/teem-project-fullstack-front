37c7824ac55e5ad64468897666c9f3ba45459190
SANYA3315 <jrchuenko88@gmail.com>
2026-05-03 13:40:57 +0300

    task-8-fix

--- app/api/home/homepublic/route.ts
+++ app/api/home/homepublic/route.ts
@@ -1,27 +1,15 @@
 import { NextResponse } from 'next/server';
+import { api } from '../../api';
 
 export async function GET() {
   try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/`,
-      {
-        method: 'GET',
-        cache: 'no-store',
-      }
-    );
+    const res = await api.get("/");
 
-    if (!res.ok) {
-      const errorData = await res.json();
-      return NextResponse.json(errorData, { status: res.status });
-    }
-
-    const data = await res.json();
-
-    return NextResponse.json(data);
-  } catch (error) {
+    return NextResponse.json(res.data, { status: res.status });
+  } catch (error: any) {
     return NextResponse.json(
       { message: 'Server error while fetching public data' },
-      { status: 500 }
+      { status: error.response?.status || 500 }
     );
   }
 }
