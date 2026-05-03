392d65479140e6e5eb70dbb0ec75aee805ceb69a
George Riabov <george.riabov3105@gmail.com>
2026-05-03 16:17:29 +0200

    header+sidebar

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,53 @@
+'use client';
+
+import css from './Header.module.css';
+import Link from 'next/link';
+import { useState } from 'react';
+
+import Modal from '../Modal/Modal';
+import Sidebar from '../Sidebar/Sidebar';
+
 export default function Header() {
-  return <p>Header</p>;
+  const [isModalNavigationOpen, setIsModalNavigationOpen] = useState(false);
+
+  const openModalNavigation = () => {
+    setIsModalNavigationOpen(true);
+  };
+
+  const closeModalNavigation = () => {
+    setIsModalNavigationOpen(false);
+  };
+
+  return (
+    <>
+      <header className={css.header}>
+        <div className={css.header_container}>
+          <div className={css.logo_container}>
+            <Link href="/" aria-label="Home" className={css.logo_link}>
+              <svg width="24" height="24">
+                <use href="/icons.svg#logo" />
+              </svg>
+              <svg width="49" height="11">
+                <use href="/icons.svg#icon-leleka" />
+              </svg>
+            </Link>
+          </div>
+
+          <div className={css.menu_container}>
+            <button className={css.menu_button} onClick={openModalNavigation}>
+              <svg width="22" height="16">
+                <use href="/icons.svg#menu" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </header>
+
+      {isModalNavigationOpen && (
+        <Modal onClose={closeModalNavigation}>
+          <Sidebar />
+        </Modal>
+      )}
+    </>
+  );
 }
--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,39 +1,25 @@
 'use client';
+
 import UserBar from '../UserBar/UserBar';
 import AuthBar from '../AuthBar/AuthBar';
 
-import { useEffect, useState } from 'react';
 import css from './Sidebar.module.css';
 import Link from 'next/link';
-import Image from 'next/image';
 import { useAuthStore } from '@/lib/store/authStore';
 
 export default function SideBar() {
   const { user } = useAuthStore();
   const isAuth = Boolean(user);
 
-  const [isDesktop, setIsDesktop] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsDesktop(window.innerWidth >= 1440);
-    };
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
   return (
-    <aside className={`${css.sidebar} ${isDesktop ? css.open : ''}`}>
+    <aside className={css.sidebar}>
       <div className={css.inner}>
-        <div className={css.header}>
-          <Link href="/" className={css.logoWrap}>
-            <svg width="32" height="32">
-              <use href="/icons.svg#logo" />
-            </svg>
-            <span className={css.logoText}>Лелека</span>
-          </Link>
-        </div>
+        <Link href="/" className={css.logoWrap}>
+          <svg width="32" height="32">
+            <use href="/icons.svg#logo" />
+          </svg>
+          <span className={css.logoText}>Лелека</span>
+        </Link>
 
         <nav className={css.nav}>
           <ul className={css.navList}>
@@ -77,37 +63,7 @@ export default function SideBar() {
 
         <div className={css.divider} />
 
-        <div className={css.footer}>
-          {!isAuth ? (
-            <div className={css.authLinks}>
-              <Link href="/register" className={css.linkSingup}>
-                Зареєструватися
-              </Link>
-              <Link href="/login" className={css.linkSingin}>
-                Увійти
-              </Link>
-            </div>
-          ) : (
-            <div className={css.userBlock}>
-              <Image
-                src={user.avatar}
-                alt="avatar"
-                width={40}
-                height={40}
-                className={css.avatar}
-              />
-              <div className={css.userText}>
-                <p>{user.name}</p>
-                <p>{user.email}</p>
-              </div>
-              <button className={css.logoutBtn}>
-                <svg width="24" height="24">
-                  <use href="/icons.svg#logout" />
-                </svg>
-              </button>
-            </div>
-          )}
-        </div>
+        <div className={css.footer}>{isAuth ? <UserBar /> : <AuthBar />}</div>
       </div>
     </aside>
   );
