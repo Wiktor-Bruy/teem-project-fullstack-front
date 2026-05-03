9d9170119934bf0daa1cb5b6f16e39832a9d7f78
Crazy_Fox <holmyliszag@ukr.net>
2026-05-03 11:41:14 +0300

    route

--- app/api/note/one/[noteId]/route.ts
+++ app/api/note/one/[noteId]/route.ts
@@ -1,6 +1,6 @@
-import { NextResponse} from 'next/server';
-import { api } from '../../api';
-import { cookies} from 'next/headers';
+import { NextResponse } from 'next/server';
+import { api } from '../../../api';
+import { cookies } from 'next/headers';
 import { isAxiosError } from 'axios';
 
 type Props = {
